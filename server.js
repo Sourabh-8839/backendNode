62c003f90d973e5253f300ab50605cedf1eccc64
Sourabh-8839 <sourabhsingh8839@gmail.com>
2023-05-16 20:46:45 +0530

    create urls

--- server.js
+++ server.js
@@ -3,16 +3,28 @@ const http = require('http');
 
 const server =http.createServer((req,res)=>{
 
- 
-   if(req.url==="/"){
-      res.end('radhe');
-      
-   }
-   if(req.url==='/about'){
-      res.end('this our short page');
+   if(req.url==='/'){
+
+      res.write('<html>')
+      res.write('<head><title>this is my first project</title></head>');
+      res.write('<body><h1>Welcome to my node js project</h1></body>');
+      res.write('</html>')
+
+      res.end();
    }
 
-    res.end(`<h1>Oops</h1>
+   if(req.url==='/home'){
+     return res.end('Welcome home');    
+   }
+   if(req.url==='/about'){
+      return res.end('Welcome to About Us page');
+   }
+
+   if(req.url==='/node'){
+      return res.end('Welcome to my Node Js project')
+   }
+
+   res.end(`<h1>Oops</h1>
       <p>we can't find that you seem it</p>
       <a href="/">back to homepage</a>
    `);
